17d3c1b6e4dd0517a8460024b9e5ac21c632ce98
= <=>
2021-01-13 01:38:09 -0500

    basic navigation of notes list. added ability to exit the program with keybindings

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use crossterm::{
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{enable_raw_mode, EnterAlternateScreen},
 };
 use std::io::{self, Write};
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
 mod db;
+mod events;
 mod note;
+mod notes_list_events;
 mod types;
 mod ui;
-mod notes_list_events;
+use notes_list_events::NoteListEvents;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     db::create_notes_table()?;
@@ -18,14 +20,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let mut list_state = NoteListEvents::new();
+
     enable_raw_mode()?;
     execute!(io::stdout(), EnterAlternateScreen)?;
-    // TODO way to exit the program using a keyboard shortcut
+    list_state.state.select(Some(0));
     loop {
         terminal.draw(|f| {
-            ui::draw(f);
+            ui::draw(f, &mut list_state);
         })?;
+
+        events::keyboard::handle_notes_list_events(&mut list_state)?;
+        list_state.notes.clear();
     }
-    /* disable_raw_mode()?;
-     * execute!(std::io::stdout(), LeaveAlternateScreen)?; */
 }
