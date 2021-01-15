66c77b500304e0772b48273f5585c2b574cb3390
= <=>
2021-01-14 22:41:42 -0500

    created new state system. list state is working, thats it. runs but doesn't have proper behavior

---
+++ src/app_state.rs
@@ -0,0 +1,43 @@
+use super::note::Note;
+use tui::widgets::ListState;
+
+#[derive(Debug, Default)]
+pub struct AppState {
+    pub list_state: ListState,
+    pub current_note_chars: Vec<char>,
+    pub notes: Vec<Note>,
+    pub cursor: usize,
+}
+
+impl AppState {
+    pub fn next_note(&mut self) {
+        if let Some(selected) = self.list_state.selected() {
+            if selected >= self.notes.len() - 1 {
+                self.list_state.select(Some(0));
+            } else {
+                self.list_state.select(Some(selected + 1));
+            }
+
+        } else {
+            self.list_state.select(Some(0));
+        }
+    }
+
+    pub fn previous_note(&mut self) {
+        if let Some(selected) = self.list_state.selected() {
+            if selected <= 0 {
+                self.list_state.select(Some(self.notes.len() - 1));
+            } else {
+                self.list_state.select(Some(selected - 1));
+            }
+
+        } else {
+            self.list_state.select(Some(0));
+        }
+    }
+
+    pub fn cursor_loc(&mut self, frame_width: usize) -> (u16, u16) {
+        let mut pos = (0, 0);
+        pos
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,13 @@ use std::io::{self, Write};
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
+mod app_state;
 mod db;
 mod events;
 mod note;
-mod notes_list_events;
-mod note_state;
 mod ui;
-use notes_list_events::NoteListEvents;
-use note_state::NoteState;
+
+use app_state::AppState;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     db::create_notes_table()?;
@@ -21,18 +20,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let mut list_state = NoteListEvents::new();
-    let mut note_state = NoteState::default();
+    let mut app_state = AppState::default();
 
     enable_raw_mode()?;
     execute!(io::stdout(), EnterAlternateScreen)?;
-    list_state.state.select(Some(0));
+    app_state.list_state.select(Some(0));
     loop {
         terminal.draw(|f| {
-            ui::draw(f, &mut list_state, &mut note_state);
+            ui::draw(f, &mut app_state);
         })?;
 
-        events::keyboard::handle_notes_list_events(&mut list_state, &mut note_state)?;
-        list_state.notes.clear();
+        events::keyboard::handle_notes_list_events(&mut app_state)?;
+        app_state.notes.clear();
     }
 }
